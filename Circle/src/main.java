63f673a3605e085458d0e2e5a3595420172f1bff
Dell pc <Dell pc@192.168.0.12>
2018-01-23 21:10:57 -0500

    Circle project

---
+++ Circle/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		 Perimeter pr = new Perimeter(40);
+		 pr.find_Area();
+		 System.out.println("Diameter"   + pr.Diameter());
+		 System.out.println("Circumference"   +pr.Circumference());
+
+	}
+
+}
